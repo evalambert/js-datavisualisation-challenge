4185aa26f9632eaef60657c95e109d508cdb7ca2
evalambert <145194730+evalambert@users.noreply.github.com>
2023-11-06 12:23:07 +0100

    graphic 2

--- main.js
+++ main.js
@@ -9,18 +9,18 @@
  // CRÉATION DIV + CANVAS //
 ///////////////////////////
 
-// Création <div> #wrapper-canvas
-const wrapperCanvas = document.createElement('div');
-wrapperCanvas.id = 'wrapper-canvas';
+// Création <DIV> #WRAPPER-CANVAS-ONE
+const wrapperCanvasOne = document.createElement('div');
+wrapperCanvasOne.id = 'wrapper-canvas-one';
 
-// Insersion <div> #wrapper-canvas
+// Insersion <DIV> #WRAPPER-CANVAS-ONE
 const tableOne = document.getElementById('table1'); 
-tableOne.parentNode.insertBefore(wrapperCanvas, tableOne); 
+tableOne.parentNode.insertBefore(wrapperCanvasOne, tableOne); 
 
-// Création <canvas>
-const canvas = document.createElement('canvas');
-canvas.id = 'myChart';
-wrapperCanvas.appendChild(canvas);
+// Création <CANVAS>
+const canvasOne = document.createElement('canvas');
+canvasOne.id = 'myChart';
+wrapperCanvasOne.appendChild(canvasOne);
 
 
   /////////////////////////////////
@@ -65,14 +65,14 @@ console.log(dataCountries);
 
 
   /////////////////////////////////
- // GRAPHIQUE CHART.JS////////////
+ // GRAPHIQUE#1 CHART.JS /////////
 /////////////////////////////////
 
 
-const ctx = document.getElementById('myChart');
+const ctxOne = document.getElementById('myChart').getContext('2d')
 
-  new Chart(ctx, {
-    type: 'bar',
+  new Chart(ctxOne, {
+    type: 'scatter',
     data: {
       labels: years,
       datasets: countries.map((country, index) => {
@@ -93,4 +93,79 @@ const ctx = document.getElementById('myChart');
 
 
 
+///////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////// TABLEAU 2  /////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+  ///////////////////////////
+ // CRÉATION DIV + CANVAS //
+///////////////////////////
+
+
+// Création <DIV> #WRAPPER-CANVAS-TWO
+const wrapperCanvasTwo = document.createElement('div');
+wrapperCanvasTwo.id = 'wrapper-canvas-two';
+
+// Insersion <DIV> #WRAPPER-CANVAS-TWO
+const tableTwo = document.getElementById('table2'); 
+tableTwo.parentNode.insertBefore(wrapperCanvasTwo, tableTwo); 
+
+// Création <CANVAS>
+const canvasTwo = document.createElement('canvas');
+canvasTwo.id = 'myChart-2';
+wrapperCanvasTwo.appendChild(canvasTwo);
+
+
+  /////////////////////////////////
+ // EXTRACTION DONNÉES TABLEAUX //
+/////////////////////////////////
+
+
+let countriesTwo = [];
+let yearsTwo = [];
+let dataCountriesTwo =[];
+
+const thElementTwo = table2.rows[0];
+for (let i = 2; i < thElementTwo.cells.length; i++) {
+    yearsTwo.push(thElementTwo.cells[i].textContent);
+}
+
+for (let j = 1; j < table2.rows.length; j++) {
+    const row = table2.rows[j];
+    countriesTwo.push(row.cells[1].textContent);
+
+    const rowData = [];
+    for (let k = 2; k < row.cells.length; k++) {
+        rowData.push(parseFloat(row.cells[k].textContent.replace(",", ".")));
+    }
+    dataCountriesTwo.push(rowData);
+}
+
+
+  /////////////////////////////////
+ // GRAPHIQUE#1 CHART.JS /////////
+/////////////////////////////////
+
+
+const ctxTwo = document.getElementById('myChart-2').getContext('2d')
+
+new Chart(ctxTwo, {
+  type: 'bar',
+  data: {
+      labels: countriesTwo,
+      datasets: yearsTwo.map((year, index) => ({
+          label: year,
+          data: dataCountriesTwo.map(row => row[index]),
+          borderWidth: 1
+      }))
+  },
+  options: {
+      scales: {
+          y: {
+              beginAtZero: true
+          }
+      }
+  }
+});
 
